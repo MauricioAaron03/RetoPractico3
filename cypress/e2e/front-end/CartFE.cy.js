0811bb2b4cc20847653b52f7bd2ce5cf4359a331
Fxtality <aarongm93@outlook.com>
2024-05-13 08:08:23 -0600

    initiall commit

--- cypress/e2e/front-end/CartFE.cy.js
+++ cypress/e2e/front-end/CartFE.cy.js
@@ -2,6 +2,7 @@ describe('Cart Feature', () => {
     it('CP-004 FE / Agregar Producto al Carrito', () => {
         cy.visit('http://www.testingyes.com/onlineshop/men/1-2-hummingbird-printed-t-shirt.html#/1-size-s/11-color-black');
         cy.contains('Add to cart').children('i').should('have.class', 'material-icons shopping-cart').click()
+        cy.screenshot();
     });
 
 });
@@ -12,6 +13,7 @@ describe('Cart Feature', () => {
         cy.contains('Add to cart').children('i').should('have.class', 'material-icons shopping-cart').click()
         cy.contains('Proceed to checkout').children('i').should('have.class', 'material-icons rtl-no-flip').click()
         cy.contains('delete').children('i').should('have.class', 'material-icons float-xs-left').click()
+        cy.screenshot();
     });
 
 });
